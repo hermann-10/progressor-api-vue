8c69a911c51b1726bacabb3a652e90817fe9009a
Hermann Manuel <hermann.manuel@hotmail.com>
2021-01-15 12:05:21 +0100

    Affichage de toutes les tâches existantes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,8 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <HelloWorld msg="Vue.js App : Progressor (compo API)" />
-    <Form @createTask="addTask" /> <!-- "@" car on écoute l'événement createTask définit dans Form.vue, lorsque l'emission de cet événement arrive, on appel la fonction addTask -->
+    <Form @createTask="addTask" />
+    <!-- "@" car on écoute l'événement createTask définit dans Form.vue, lorsque l'emission de cet événement arrive, on appel la fonction addTask -->
   </div>
 </template>
 
@@ -10,16 +11,18 @@
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
 import Form from "@/components/Form.vue";
+import tasksService from "@/services/tasks.js";
 
 export default {
   name: "Home",
   components: {
     HelloWorld,
-    Form,
+    Form
   },
-  setup(){
-    function addTask(data){
-      console.log('Home.vue | addTask()', data);
+  setup() {
+    function addTask(data) {
+      console.log("Home.vue | addTask()", data);
+      tasksService.create(data);
     }
 
     //Pour utiliser addTask dans le template, il faut bien penser à retourner un objet qui comporte une propriété addTask
--- src/views/Tasks.vue
+++ src/views/Tasks.vue
@@ -1,13 +1,32 @@
 <template>
   <h3>Toutes les tâches</h3>
-  <div></div>
+ <div v-if="tasks.length > 0">
+  <div class="task" v-for="task in tasks" :key="task.id">
+    <h3>{{ task.name }}</h3>
+    <p>{{ task.description }}</p>
+    <p>Echéance : {{ task.temporality }}</p>
+  </div>
+ </div> 
 </template>
 
 <script>
+import { ref } from "vue";
+import tasksService from "@/services/tasks.js";
 export default {
-    name: 'Tasks',
+  name: "Tasks",
+  setup() {
+    const tasks = ref([]);
+    tasks.value = tasksService.read();
+
+    return { tasks };
+  }, 
 };
 </script>
 
-<style>
+<style scoped>
+    .task{
+        margin: 10px 15px;
+        border: 3px solid #42b983;
+        border-radius: 5px;
+    }
 </style>
